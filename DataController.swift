d6c6d6f9a28a7cc432292aaa6147a724b48ea1c6
Michael Rowe <support@theapapp.com>
2023-05-21 14:30:56 -0400

    Typo in name

--- DataController.swift
+++ DataController.swift
@@ -15,7 +15,7 @@ class DataController: ObservableObject {
         container = NSPersistentContainer(name: "Model") // this is the data model
         container.viewContext.automaticallyMergesChangesFromParent = true
         
-        let groupID = "group.com.theappap.spacedout2"
+        let groupID = "group.com.theapapp.spacedout2"
         
         if let url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: groupID) {
             container.persistentStoreDescriptions.first?.url = url.appending(path: "Model.sqlite")  // this is the live user data - the names don't have to match
